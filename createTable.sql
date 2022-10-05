267cc90cc55417cc744f5b64b7e863db0bdd8e18
Leigh Hunt <leigh@venari.co.nz>
2022-10-05 20:43:39 +1300

    Changing field orders back to original

--- createTable.sql
+++ createTable.sql
@@ -1,5 +1,5 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE `stats` (`id` INTEGER PRIMARY KEY, `timestamp` DATETIME, `generated` DATETIME, `qr_code_scans_today` INTEGER, `manual_entries_today` INTEGER, `people_with_bluetooth_tracing_active_today` INTEGER, `bluetooth_tracing_codes_uploaded_today` INTEGER, `contacts_notified_by_bluetooth_today` INTEGER, `all_time_app_registrations` INTEGER, `all_time_app_registrations_daily_change` VARCHAR(255), `all_time_posters_created` INTEGER, `all_time_posters_created_daily_change` VARCHAR(255), `JSON` VARCHAR(255), `createdAt` DATETIME, `updatedAt` DATETIME);
+CREATE TABLE `stats` (`id` INTEGER PRIMARY KEY, `timestamp` DATETIME, `generated` DATETIME, `qr_code_scans_today` INTEGER, `manual_entries_today` INTEGER, `people_with_bluetooth_tracing_active_today` INTEGER, `all_time_app_registrations` INTEGER, `all_time_app_registrations_daily_change` VARCHAR(255), `all_time_posters_created` INTEGER, `all_time_posters_created_daily_change` VARCHAR(255), `JSON` VARCHAR(255), `createdAt` DATETIME, `updatedAt` DATETIME, `bluetooth_tracing_codes_uploaded_today` INTEGER, `contacts_notified_by_bluetooth_today` INTEGER);
 COMMIT;
 
